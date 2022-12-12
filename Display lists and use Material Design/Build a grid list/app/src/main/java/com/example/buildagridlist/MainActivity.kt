34eebdd3d45b8c3bb0d07b2c6d31842735aa8bf8
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2022-12-12 21:52:49 +0700

    Continue show grid list

--- Display lists and use Material Design/Build a grid list/app/src/main/java/com/example/buildagridlist/MainActivity.kt
+++ Display lists and use Material Design/Build a grid list/app/src/main/java/com/example/buildagridlist/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.buildagridlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buildagridlist.data.Datasource
+import com.example.buildagridlist.model.Topic
 import com.example.buildagridlist.ui.theme.BuildAGridListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TopicGrid()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        modifier = modifier.padding(8.dp),
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        content = {
+            items(Datasource.topics) { topic -> TopicCard(topic = topic) }
+        }
+    )
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(/*elevation = 4.dp*/) {
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.imageRes),
+                    contentDescription = null,
+                    modifier = modifier
+                        .size(68.dp, 68.dp)
+                        .aspectRatio(1f)
+                )
+            }
+
+            Column() {
+                Text(
+                    text = stringResource(id = topic.name),
+                    modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 8.dp),
+                    style = MaterialTheme.typography.body2
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 8.dp)
+                            .size(12.dp)
+                    )
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TopicCardPreview() {
-    BuildAGridListTheme {
-        Greeting("Android")
-    }
+
+    TopicCard(Topic(R.string.architecture, 58, R.drawable.architecture))
 }
