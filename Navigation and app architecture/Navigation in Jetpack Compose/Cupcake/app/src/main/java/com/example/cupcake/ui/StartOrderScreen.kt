0e66c0826da24a0d336f0f5e370f95d484006e5c
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2022-12-22 16:46:45 +0700

    App Cupcake

---
+++ Navigation and app architecture/Navigation in Jetpack Compose/Cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -0,0 +1,70 @@
+package com.example.cupcake.ui
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cupcake.R
+import com.example.cupcake.data.DataSource.quantityOptions
+
+
+@Composable
+fun StartOrderScreen(
+    quantityOptions: List<Pair<Int, Int>>,
+    // TODO: add onNextButtonClicked
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Spacer(modifier = modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = R.drawable.cupcake),
+            contentDescription = null,
+            modifier = Modifier.width(300.dp)
+        )
+        Spacer(modifier = modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.order_cupcakes),
+            style = MaterialTheme.typography.h4
+        )
+        Spacer(modifier = modifier.height(8.dp))
+        quantityOptions.forEach { item ->
+            SelectQuantityButton(
+                labelResourceId = item.first,
+                onClick = { /*TODO*/ }
+            )
+        }
+    }
+}
+
+@Composable
+fun SelectQuantityButton(
+    @StringRes labelResourceId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+        Text(stringResource(labelResourceId))
+    }
+}
+
+@Preview
+@Composable
+fun StartOrderPreview() {
+    StartOrderScreen(quantityOptions = quantityOptions)
+}
