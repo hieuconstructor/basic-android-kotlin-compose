c9c9cd4bb6f821098f91aa9d78fab8dbd2df49ce
Nguyễn Viết Hiếu <hieuconstructor@gmail.com>
2022-12-27 21:35:34 +0700

    Update adaptive layout for app reply
    Tạo bố cục thích ứng cho mẫu chế độ xem danh sách bằng Jetpack Compose

--- Navigation and app architecture/Adapt for different screen sizes/Reply App/app/src/main/java/com/example/replyapp/ui/ReplyHomeContent.kt
+++ Navigation and app architecture/Adapt for different screen sizes/Reply App/app/src/main/java/com/example/replyapp/ui/ReplyHomeContent.kt
@@ -1,5 +1,6 @@
 package com.example.replyapp.ui
 
+import android.app.Activity
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -11,41 +12,113 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.replyapp.R
 import com.example.replyapp.data.Email
-import com.example.replyapp.ui.theme.ReplyAppTheme
+import com.example.replyapp.data.local.LocalAccountsDataProvider
 
+/**
+ * Component that displays a single pane of list of emails
+ */
 @Composable
 fun ReplyListOnlyContent(
     replyUiState: ReplyUiState,
+    onEmailCardPressed: (Email) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val emails = replyUiState.currentMailboxEmails
 
     LazyColumn(modifier = modifier.padding(horizontal = 16.dp)) {
-        item { }
+        item {
+            ReplyHomeTopBar(modifier = Modifier.fillMaxWidth())
+        }
         items(emails, key = { email -> email.id }) { email ->
-            ReplyEmailListItem(email = email)
+            ReplyEmailListItem(
+                email = email,
+                onCardClick = {
+                    onEmailCardPressed(email)
+                }
+            )
         }
     }
 }
 
+/**
+ * Component that displays two panes of list of emails and email details
+ */
+@Composable
+fun ReplyListAndDetailContent(
+    replyUiState: ReplyUiState,
+    onEmailCardPressed: (Email) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val emails = replyUiState.currentMailboxEmails
+    Row(modifier = modifier) {
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 16.dp, top = 20.dp)
+        ) {
+            items(emails, key = { email -> email.id }) { email ->
+                ReplyEmailListItem(
+                    email = email,
+                    onCardClick = {
+                        onEmailCardPressed(email)
+                    }
+                )
+            }
+        }
+        val activity = LocalContext.current as Activity
+        ReplyDetailsScreen(
+            replyUiState = replyUiState,
+            modifier = Modifier.weight(1f),
+            onBackPressed = { activity.finish() }
+        )
+    }
+}
+
+/**
+ * Component that displays a single email list item
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ReplyEmailListItem(email: Email, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.padding(vertical = 4.dp), onClick = { /*TODO*/ }) {
+fun ReplyEmailListItem(
+    email: Email,
+    onCardClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.padding(vertical = 4.dp),
+        onClick = onCardClick
+    ) {
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(20.dp)
         ) {
-            ReplyEmailItemHeader(email = email)
+            ReplyEmailItemHeader(email)
+            Text(
+                text = email.subject,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+            )
+            Text(
+                text = email.body,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 2,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
@@ -64,7 +137,10 @@ private fun ReplyEmailItemHeader(email: Email, modifier: Modifier = Modifier) {
                 .padding(horizontal = 12.dp, vertical = 4.dp),
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = email.sender.firstName, style = MaterialTheme.typography.labelMedium)
+            Text(
+                text = email.sender.firstName,
+                style = MaterialTheme.typography.labelMedium
+            )
             Text(
                 text = email.createdAt,
                 style = MaterialTheme.typography.labelMedium,
@@ -74,16 +150,61 @@ private fun ReplyEmailItemHeader(email: Email, modifier: Modifier = Modifier) {
     }
 }
 
+/**
+ * Component that displays profile image
+ */
 @Composable
 fun ReplyProfileImage(
     @DrawableRes drawableResource: Int,
     description: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Image(
-        painter = painterResource(id = drawableResource),
+        modifier = modifier.clip(CircleShape),
+        painter = painterResource(drawableResource),
         contentDescription = description,
-        modifier = modifier.clip(CircleShape)
     )
 }
 
+/**
+ * Component that displays Reply logo
+ */
+@Composable
+fun ReplyLogo(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary
+) {
+    Image(
+        painter = painterResource(R.drawable.logo),
+        contentDescription = stringResource(R.string.logo),
+        colorFilter = ColorFilter.tint(color),
+        modifier = modifier
+    )
+}
+
+/**
+ * Component that displays top bar.
+ */
+@Composable
+private fun ReplyHomeTopBar(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        ReplyLogo(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(start = 4.dp)
+        )
+        ReplyProfileImage(
+            drawableResource = LocalAccountsDataProvider.userAccount.avatar,
+            description = stringResource(R.string.profile),
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .size(48.dp)
+        )
+    }
+}
